46c91faf5f18374a53815b0a4dbc4353fb6ddbd7
Math-Barros <rm80802@fiap.com.br]>
2021-10-22 13:25:42 -0300

    Update 1.0

--- android/app/src/main/kotlin/com/example/shop_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sirvice_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/pages/start.dart
@@ -0,0 +1,133 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:app_frontend/components/modals/internetConnection.dart';
+import 'package:app_frontend/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:app_frontend/services/userService.dart';
+import 'package:app_frontend/sizeConfig.dart';
+
+class Start extends StatelessWidget {
+  final UserService _userService = new UserService();
+
+  validateToken(context) async {
+    bool connectionStatus = await _userService.checkInternetConnectivity();
+
+    if (connectionStatus) {
+      await Firebase.initializeApp();
+      final storage = new FlutterSecureStorage();
+      String value = await storage.read(key: 'idToken');
+      if (value != null) {
+        String decodedToken = _userService.validateToken(value);
+        if (decodedToken != null) {
+          Navigator.of(context).pushReplacementNamed('/home');
+        } else {
+          Navigator.of(context).pushReplacementNamed('/login');
+        }
+      } else {
+        Navigator.of(context).pushReplacementNamed('/login');
+      }
+    } else {
+      internetConnectionDialog(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: SingleChildScrollView(
+          child: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: SizeConfig.safeBlockVertical * 8),
+            Image.asset('assets/white_logo.png',
+                height: SizeConfig.safeBlockVertical * 20),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
+              child: FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Text('Sirvice',
+                    style: TextStyle(
+                        fontSize: SizeConfig.safeBlockHorizontal * 8,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'NovaSquare',
+                        color: Colors.white)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: SizeConfig.safeBlockVertical * 3,
+                horizontal: SizeConfig.safeBlockHorizontal * 12.5,
+              ),
+              child: Text(
+                'Encontre o melhor profissional freelancer com nosso app',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: 'NovaSquare',
+                    color: Colors.white,
+                    fontSize: SizeConfig.safeBlockHorizontal * 4.6,
+                    letterSpacing: 1.0),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: SizeConfig.safeBlockVertical * 5),
+              child: ButtonTheme(
+                minWidth: SizeConfig.screenWidth - 180,
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
+                  padding: EdgeInsets.symmetric(
+                      vertical: SizeConfig.safeBlockVertical * 3),
+                  color: Colors.black,
+                  textColor: Colors.white,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontFamily: 'NovaSquare',
+                      fontSize: SizeConfig.safeBlockHorizontal * 4.5,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  onPressed: () {
+                    validateToken(context);
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: SizeConfig.safeBlockVertical * 3.0),
+              child: ButtonTheme(
+                minWidth: SizeConfig.screenWidth - 180,
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                      side: BorderSide(color: Colors.black, width: 2.6)),
+                  padding: EdgeInsets.symmetric(
+                      vertical: SizeConfig.safeBlockVertical * 3),
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        fontFamily: 'NovaSquare',
+                        fontSize: SizeConfig.safeBlockHorizontal * 4.5,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.0),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/signup');
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
